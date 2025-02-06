e8bd4c7c2cb5f40ea4387811493646417ff06588
Majumder99 <souravmajumderk99@gmail.com>
2025-02-07 00:37:38 +0600

    Sparse Arrays

---
+++ Hackerrank/Sparse Arrays.py
@@ -0,0 +1,58 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY strings
+#  2. STRING_ARRAY queries
+#
+
+def matchingStrings(strings, queries):
+    # Write your code here
+    dict = {}
+    for i in strings:
+        if i in dict:
+            dict[i] += 1
+        else:
+            dict[i] = 1
+    res = []
+    for i in range(len(queries)):
+        if queries[i] in dict:
+            res.append(dict[queries[i]])
+        else:
+            res.append(0)
+    return res
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    strings_count = int(input().strip())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
