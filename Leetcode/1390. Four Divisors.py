0566f1a9c9e44602b5ca4415b59044e43de59738
Majumder99 <souravmajumderk99@gmail.com>
2025-02-07 00:26:07 +0600

    1390. Four Divisors

---
+++ Leetcode/1390. Four Divisors.py
@@ -0,0 +1,19 @@
+class Solution:
+    def sumFourDivisors(self, nums: List[int]) -> int:
+        def get_divisors(n):
+            divisors = set()
+            for i in range(1, n+1):
+                if n % i == 0 and i not in divisors:
+                    divisors.add(i)
+                    divisors.add(n//i)
+        
+            return divisors
+
+        for i in range(len(nums)):
+            divisors = get_divisors(nums[i])
+            if len(divisors) != 4:
+                nums[i] = 0
+            else:
+                nums[i] = sum(divisors)
+                
+        return sum(nums)
