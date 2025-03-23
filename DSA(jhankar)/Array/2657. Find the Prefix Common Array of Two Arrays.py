09f6bb52306d4d153b1af2cac096f97efcc24925
Sourav <souravmajumderk99@gmail.com>
2025-03-23 22:31:29 +0600

    2657. Find the Prefix Common Array of Two Arrays

--- DSA(jhankar)/Array/2657. Find the Prefix Common Array of Two Arrays.py
+++ DSA(jhankar)/Array/2657. Find the Prefix Common Array of Two Arrays.py
@@ -5,20 +5,18 @@ class Solution:
             C.append(1)
         else: 
             C.append(0)
-        
 
         for i in range(1, len(A)):
             a = sorted(A[0:i+1])
             b = sorted(B[0:i+1])
             counter = 0
-            print(a)
-            print(b)
-            print(counter)
             for j in range(len(a)):
-                if a[j] == b[j]:
-                    counter += 1
+                first = a[j]
+                for k in range(len(a)):
+                    if first == b[k]:
+                        counter += 1
             C.append(counter)
         return C
 
 s = Solution()
-s.findThePrefixCommonArray([1,3,2,4], [3,1,2,4])
+s.findThePrefixCommonArray([1,2,3], [2,3,1])
