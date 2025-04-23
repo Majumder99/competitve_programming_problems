25f3d932953538c0d77c881b6317cc66fe3b730b
Sourav <souravmajumderk99@gmail.com>
2025-04-23 21:36:55 +0600

    2762. Continuous Subarrays

--- DSA(jhankar)/Queue/2762. Continuous Subarrays.py
+++ DSA(jhankar)/Queue/2762. Continuous Subarrays.py
@@ -1,11 +1,26 @@
+from collections import deque
 class Solution:
     def continuousSubarrays(self, nums: List[int]) -> int:
         start = 0
         result = 0
+        max_deque = deque()
+        min_deque = deque()
 
         for end in range(len(nums)):
-            while max(nums[start:end+1]) - min(nums[start:end+1]) > 2:
-                start += 1
+            while max_deque and nums[max_deque[-1]] <= nums[end]:
+                max_deque.pop()
+            max_deque.append(end)
+
+            while min_deque and nums[min_deque[-1]] >= nums[end]:
+                min_deque.pop()
+            min_deque.append(end)
+
+            while nums[max_deque[0]] - nums[min_deque[0]] > 2:
+                start += 1 
+                if max_deque[0] < start:
+                    max_deque.popleft()
+                if min_deque[0] < start:
+                    min_deque.popleft()
             result += (end - start + 1)
         return result
 
